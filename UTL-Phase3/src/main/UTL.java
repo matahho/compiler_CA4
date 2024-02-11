f665940935605e4a8a4164159ab6e0ed56a8fc63
matahho <mata1381@gmail.com>
2024-02-11 14:40:35 +0330

    add Code genetarion obj to UTL class

--- UTL-Phase3/src/main/UTL.java
+++ UTL-Phase3/src/main/UTL.java
@@ -5,6 +5,7 @@ import main.compileError.CompileError;
 import main.visitor.astPrinter.ASTPrinter;
 import main.visitor.nameAnalyzer.NameAnalyzer;
 import main.visitor.typeAnalyzer.TypeAnalyzer;
+import main.visitor.codeGenerator.CodeGenerator;
 import org.antlr.v4.runtime.*;
 import parsers.*;
 
@@ -12,18 +13,24 @@ import java.io.IOException;
 
 public class UTL {
     public static void main(String[] args) throws IOException {
+
         CharStream reader = CharStreams.fromFileName(args[0]);
         UTLLexer lexer = new UTLLexer(reader);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         UTLParser parser = new UTLParser(tokens);
         Program program = parser.program().pro;
-        NameAnalyzer nameAnalyzer = new NameAnalyzer();
-        nameAnalyzer.visit(program);
-        TypeAnalyzer typeAnalyzer = new TypeAnalyzer();
-        typeAnalyzer.visit(program);
-        if (!typeAnalyzer.typeErrors.isEmpty()){
-            for(CompileError compileError: typeAnalyzer.typeErrors)
-                System.out.println(compileError.getMessage());
-        }
+//        NameAnalyzer nameAnalyzer = new NameAnalyzer();
+//        nameAnalyzer.visit(program);
+//        TypeAnalyzer typeAnalyzer = new TypeAnalyzer();
+//        typeAnalyzer.visit(program);
+//        if (!typeAnalyzer.typeErrors.isEmpty()) {
+//            for (CompileError compileError : typeAnalyzer.typeErrors)
+//                System.out.println(compileError.getMessage());
+//        }
+
+        CodeGenerator cg = new CodeGenerator();
+        cg.visit(program);
+
+
     }
 }
